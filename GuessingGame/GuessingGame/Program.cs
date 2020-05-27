8e0ceff89daf75a136df441dffdb24dcc352e535
Maryna Haidashevska <53351149+h-maryna@users.noreply.github.com>
2020-05-26 20:40:53 -0500

    Guessing game built in .NET

---
+++ GuessingGame/GuessingGame/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "apple";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+            while (guess != secretWord && !outOfGuesses)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+                
+            }
+            if (outOfGuesses)
+            {
+                Console.WriteLine("You Lose!");
+            }
+            else
+            {
+                Console.WriteLine("You Win!");
+            }
+            Console.ReadLine();
+        }       
+    }
+}
