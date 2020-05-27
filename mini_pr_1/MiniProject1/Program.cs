535f3950606bbcf0c0ab7c92062924c60b5335ca
Maryna Haidashevska <53351149+h-maryna@users.noreply.github.com>
2020-05-26 19:44:57 -0500

    Simple calculator built in .net was added

---
+++ mini_pr_1/MiniProject1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MiniProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 10;
+            int sum = num1 - num2;
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please input a first number");
+
+            string inputedNum1 = Console.ReadLine();
+            int num3 = Int32.Parse(inputedNum1);
+
+            Console.WriteLine("Please enter a second number");
+
+            string inputedNum2 = Console.ReadLine();
+            int num4 = Int32.Parse(inputedNum2);
+
+            Console.WriteLine("Please choose what type of math operation you want to do (1 - Add, 2 - Substract, 3 -Multiply, 4 -Divide)");
+            
+            string inputedOperat = Console.ReadLine();
+            int num5 = Int32.Parse(inputedOperat);
+
+            switch (num5)
+            {
+                case 1:
+                    Console.WriteLine(num3 + num4);
+                    break;
+                case 2:
+                    Console.WriteLine(num3 - num4);
+                    break;
+                case 3:
+                    Console.WriteLine(num3 * num4);
+                    break;
+                case 4:
+                    Console.WriteLine(num3 / num4);
+                    break;
+                default:
+                    Console.WriteLine("Please choose one of the Math operations");
+                    break;
+            }
+        }
+    }
+}
