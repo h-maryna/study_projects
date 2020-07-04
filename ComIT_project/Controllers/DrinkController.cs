89bd06c7afb9d23b443f692006eee138e3181ed5
Maryna Haidashevska <53351149+h-maryna@users.noreply.github.com>
2020-07-04 11:52:48 -0500

    ComIT project created and Controllers added

---
+++ ComIT_project/Controllers/DrinkController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DrinkAndGo.Data.Interfaces;
+using DrinkAndGo.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DrinkAndGo.Controllers
+{
+    public class DrinkController : Controller
+    {
+        private readonly IDrinkRepository _drinkRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public DrinkController(IDrinkRepository drinkRepository, ICategoryRepository categoryRepository)
+        {
+            _drinkRepository = drinkRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            ViewBag.Name = "DotNet, How?";
+            var drinks = _drinkRepository.Drinks;
+            return View(drinks);
+        }
+    }
+}
